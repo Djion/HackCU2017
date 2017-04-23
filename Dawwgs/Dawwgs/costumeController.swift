a9467354dd5f9d1a7cd763a3a3a3943a4e7791c8
Djion <alri7215@colorado.edu>
2017-04-22 23:17:51 -0600

    added a page

---
+++ Dawwgs/Dawwgs/costumeController.swift
@@ -0,0 +1,13 @@
+//
+//  costumeController.swift
+//  Dawwgs
+//
+//  Created by Alex Ring on 4/22/17.
+//  Copyright © 2017 Eudaimonia. All rights reserved.
+//
+
+import Foundation
+
+class costumeController: UITableViewController{
+    
+}
